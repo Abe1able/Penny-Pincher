8975e14fbecf3349c710d8d1adbdb0f76a581b89
Abe1able <abel.workmail@gmail.com>
2023-07-14 09:35:59 +0300

    Create the migration files

---
+++ db/migrate/20230714063249_create_expenditures.rb
@@ -0,0 +1,11 @@
+class CreateExpenditures < ActiveRecord::Migration[7.0]
+  def change
+    create_table :expenditures do |t|
+      t.string :name
+      t.float :amount
+      t.references :author, foreign_key: { to_table: :users }, null: false
+
+      t.timestamps
+    end
+  end
+end
