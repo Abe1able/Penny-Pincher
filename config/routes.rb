185dae948b62ec1f7ff8398ffc0b8497365bf5f7
Abe1able <abel.workmail@gmail.com>
2023-07-14 11:12:20 +0300

    Add functionality for the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'splash_screen/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "splash_screen#index"
+  resources :categories, only: [:index, :new, :create, :show, :destroy] do
+    resources :expenditures, only: [:index, :new, :create, :show, :destroy]
+  end
 end
